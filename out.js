c9bd2fcb582c72be0c5beb7c7cddbf77a37ec906
Oj <oj@oojmed.com>
2022-05-25 20:07:31 +0100

    powercord/components: add spinner

--- out.js
+++ out.js
@@ -723,6 +723,8 @@ module.exports = {
 };`,
   'powercord/components': `module.exports = {
   Tooltip: goosemod.webpackModules.findByProps('TooltipContainer').TooltipContainer,
+  Spinner: goosemod.webpackModules.findByProps('Spinner').Spinner,
+
   AsyncComponent: { // perfect
     from: (comp) => comp
   }
--- src/builtins/powercord/components/index.js
+++ src/builtins/powercord/components/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   Tooltip: goosemod.webpackModules.findByProps('TooltipContainer').TooltipContainer,
+  Spinner: goosemod.webpackModules.findByProps('Spinner').Spinner,
+
   AsyncComponent: { // perfect
     from: (comp) => comp
   }
