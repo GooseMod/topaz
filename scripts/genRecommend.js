57ebf6657aa969d7e846be5800cf8ab2507072f1
Oj <oj@oojmed.com>
2022-05-28 22:54:00 +0100

    scripts/genRec: redo

--- scripts/genRecommend.js
+++ scripts/genRecommend.js
@@ -32,32 +32,52 @@ const pc = {
   ]
 };
 
-const gen_pc = async (place, theme) => { // just repo or url
+const bd = {
+  plugins: [
+    'https://github.com/dylan-dang/BetterDiscordPlugins/blob/main/PreviewMessageLinks.plugin.js'
+  ]
+};
+
+const getManifest_pc = async (place, theme) => { // just repo or url
   const manifestName = theme ? 'powercord_manifest.json' : 'manifest.json';
-  console.log(place, manifestName);
+
+  console.log(place);
+
   let manifestUrl = place + '/' + manifestName;
   if (!place.startsWith('http')) manifestUrl = `https://raw.githubusercontent.com/${place}/HEAD/${manifestName}`;
 
-  const manifest = await (await fetch(manifestUrl)).json();
+  return await (await fetch(manifestUrl)).json();
+};
 
-  // Remove unneeded manifest entries to reduce size
-  delete manifest['consent'];
-  delete manifest['license'];
+const getManifest_bd = async (place, theme) => { // .plugin.js url
+  const code = await (await fetch(place.replace('github.com', 'raw.githubusercontent.com').replace('blob/', ''))).text();
 
-  return {
-    place,
-    manifest
-  };
+  console.log(place);
+
+  return [...code.matchAll(/^ \* @([^ ]*) (.*)/gm)].reduce((a, x) => { a[x[1]] = x[2]; return a; }, {});
 };
 
 
 (async () => {
-  const out = JSON.stringify({
-    plugins: await Promise.all([ ...pc.plugins.map(async x => await gen_pc(x, false)) ]),
-    themes: await Promise.all([ ...pc.themes.map(async x => await gen_pc(x, true)) ])
-  });
+  let out = {
+    plugins: {},
+    themes: {}
+  };
+
+  for (const place of pc.plugins) {
+    const manifest = await getManifest_pc(place, false);
+    out.plugins[`PC%${manifest.name}%${manifest.author}`] = place;
+  }
+
+  for (const place of pc.themes) {
+    const manifest = await getManifest_pc(place, true);
+    out.themes[`PC%${manifest.name}%${manifest.author}`] = place;
+  }
+
+  for (const place of bd.plugins) {
+    const manifest = await getManifest_bd(place, false);
+    out.plugins[`BD%${manifest.name}%${manifest.author}`] = place;
+  }
   
   console.log(out);
-  
-  writeFileSync(join(__dirname, '../recommended.json'), out);
 })();
