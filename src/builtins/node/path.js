a363c10cba9017a6701286492aef8509d86b3bfe
Oj <oj@oojmed.com>
2022-06-07 08:31:31 +0100

    node/path: make resolve more consistent with relativity

--- src/builtins/node/path.js
+++ src/builtins/node/path.js
@@ -1,6 +1,7 @@
 const resolve = (x) => {
   let ind;
-  x = x.replaceAll('./', '/').replaceAll('//', ''); // example/./test -> example/test
+  if (x.startsWith('./')) x = x.substring(2);
+  x = x.replaceAll('./', '/').replaceAll('//', '/'); // example/./test -> example/test
 
   while (ind = x.indexOf('../') !== -1) x = x.slice(0, ind) + x.slice(ind + 3); // example/test/../sub -> example/sub
 
