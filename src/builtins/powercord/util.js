d74581e43008a0ec887ac6b924ef755536f0387b
CanadaHonk <oj@oojmed.com>
2022-07-04 19:23:22 +0100

    pc/util: add wrapInHooks

--- src/builtins/powercord/util.js
+++ src/builtins/powercord/util.js
@@ -39,5 +39,39 @@ module.exports = {
 
   },
 
+  wrapInHooks: method => (...args) => {
+    const { React } = goosemod.webpackModules.common;
+
+    const { current } = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentDispatcher;
+
+    const useMemo = current.useMemo;
+    const useState = current.useState;
+    const useReducer = current.useReducer;
+    const useEffect = current.useEffect;
+    const useLayoutEffect = current.useLayoutEffect;
+    const useRef = current.useRef;
+    const useCallback = current.useCallback;
+
+    current.useMemo = method => method();
+    current.useState = val => [ val, () => null ];
+    current.useReducer = val => [ val, () => null ];
+    current.useEffect = () => null;
+    current.useLayoutEffect = () => null;
+    current.useRef = () => ({});
+    current.useCallback = cb => cb;
+
+    const res = method(...args);
+
+    current.useMemo = useMemo;
+    current.useState = useState;
+    current.useReducer = useReducer;
+    current.useEffect = useEffect;
+    current.useLayoutEffect = useLayoutEffect;
+    current.useRef = useRef;
+    current.useCallback = useCallback;
+
+    return res;
+  },
+
   ...goosemod.reactUtils // Export GooseMod React utils
 };
