b828e58ab17b52c54621807d667146b92a313f52
CanadaHonk <oj@oojmed.com>
2022-07-23 23:22:22 +0100

    pc/modal: ignore changelog modals

--- src/builtins/powercord/modal.js
+++ src/builtins/powercord/modal.js
@@ -3,10 +3,14 @@ const Modal = goosemod.webpackModules.findByProps('ModalRoot');
 
 let lastId;
 module.exports = {
-  open: (comp) => lastId = modalManager.openModal(props => React.createElement(Modal.ModalRoot, {
-    ...props,
-    className: 'topaz-pc-modal-jank'
-  }, React.createElement(comp))),
+  open: (comp) => {
+    if (comp.toString().toLowerCase().includes('changelog')) return console.warn('Ignoring modal open for changelog');
+
+    return lastId = modalManager.openModal(props => React.createElement(Modal.ModalRoot, {
+      ...props,
+      className: 'topaz-pc-modal-jank'
+    }, React.createElement(comp)))
+  },
 
   close: () => modalManager.closeModal(lastId),
 
