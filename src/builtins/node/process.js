c2d4b50c6231a89b6ba40784f0979bb02ad76f46
CanadaHonk <oj@oojmed.com>
2022-08-02 23:44:20 +0100

    process: add hrtime

--- src/builtins/node/process.js
+++ src/builtins/node/process.js
@@ -2,5 +2,15 @@ module.exports = {
   platform: 'linux',
   env: {
     HOME: '/home/topaz'
+  },
+
+  hrtime: (toDiff) => {
+    const stamp = Math.floor(performance.now());
+    const sec = Math.floor(stamp / 1000);
+    const nano = (stamp % 1000) * 1000 * 1000;
+
+    if (!toDiff) return [ sec, nano ];
+
+    return [ Math.abs(sec - toDiff[0]), Math.abs(nano - toDiff[1]) ]
   }
 };
