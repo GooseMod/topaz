5450a6a4559016f69b989fe014a178ae1d9cb0c6
CanadaHonk <oj@oojmed.com>
2022-08-02 20:49:34 +0100

    bundler: fix formatting in default export

--- src/index.js
+++ src/index.js
@@ -392,8 +392,8 @@ const makeChunk = async (root, p) => {
 let ${id} = {};
 (() => { // MAP_START|${finalPath}
 ` + code
-      .replace('module.exports =', `${id}=`)
-      .replace('export default', `${id}=`)
+      .replace('module.exports =', `${id} =`)
+      .replace('export default', `${id} =`)
       .replaceAll(/(module\.)?exports\.(.*?)=/g, (_, _mod, key) => `${id}${key}=`)
       .replaceAll(/export const (.*?)=/g, (_, key) => `${id}.${key}=`)
       .replaceAll(/export function (.*?)\(/g, (_, key) => `${id}.${key} = function(`) + `
