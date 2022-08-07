e9a47fe84620189fda5fc5862c49657aee6086b1
CanadaHonk <oj@oojmed.com>
2022-08-07 15:14:11 +0100

    node/path: update to new resolvePath

--- src/builtins/node/path.js
+++ src/builtins/node/path.js
@@ -1,14 +1,17 @@
 const resolve = (x) => {
   let ind;
   if (x.startsWith('./')) x = x.substring(2);
-  x = x.replaceAll('./', '/').replaceAll('//', '/'); // example/./test -> example/test
+  x = x.replaceAll('/./', '/').replaceAll('//', '/'); // example/./test -> example/test
 
-  while (ind = x.indexOf('../') !== -1) x = x.slice(0, ind) + x.slice(ind + 3); // example/test/../sub -> example/sub
+  while ((ind = x.indexOf('../')) !== -1) {
+      const priorSlash = x.lastIndexOf('/', ind - 4);
+      x = x.slice(0, priorSlash === -1 ? 0 : (priorSlash + 1)) + x.slice(ind + 3); // example/test/../sub -> example/sub
+  }
 
   return x;
 };
 
 module.exports = {
   join: (...parts) => resolve(parts.join('/')),
-  resolve: (...parts) => resolve(parts.join('/')) // todo: implement resolve properly (root / overwrite)
+  resolve: (...parts) => resolve(parts.join('/')), // todo: implement resolve properly (root / overwrite)
 };
